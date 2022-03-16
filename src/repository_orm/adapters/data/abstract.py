db4786929af6479b7bbeaeaa5e720f4eaa085004
Lyz <lyz@riseup.net>
2022-03-16 18:43:34 +0100

    fix: return empty desired_values in search and all

--- src/repository_orm/adapters/data/abstract.py
+++ src/repository_orm/adapters/data/abstract.py
@@ -178,9 +178,9 @@ class Repository(abc.ABC):
 
         entities = sorted(self._all(models))
 
-        # ignore: the type cannot be List[Entity] but it can, I don't know how to fix
-        # this
-        self.cache.add(entities)  # type: ignore
+        for entity in entities:
+            entity.clear_defined_values()
+            self.cache.add(entity)
 
         return entities
 
@@ -234,9 +234,9 @@ class Repository(abc.ABC):
                 raise error
             found_entities = []
 
-        # ignore: the type cannot be List[Entity] but it can, I don't know how to fix
-        # this
-        self.cache.add(found_entities)  # type: ignore
+        for entity in found_entities:
+            entity.clear_defined_values()
+            self.cache.add(entity)
 
         return found_entities
 
